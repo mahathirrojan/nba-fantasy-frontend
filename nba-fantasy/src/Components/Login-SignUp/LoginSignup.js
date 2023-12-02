58c12a657f27551e8d8c7606fd760f16eb60f233
mahathirrojan <torbengalifreind@gmail.com>
2023-12-02 00:55:59 -0500

    added all the basic login page

--- nba-fantasy/src/Components/Login-SignUp/LoginSignup.js
+++ nba-fantasy/src/Components/Login-SignUp/LoginSignup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './LoginSignup.css'
+import { useState } from 'react'
 
 
 import user_icon from '../Assets/person.png'
@@ -7,30 +8,35 @@ import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
 export const LoginSignup = () => {
+
+    const [action,setAction] = useState("Sign Up");
+
   return (
     <div className='container'>
         <div className='header'>
-            <div className="text">SignUp</div>
+            <div className="text">{action}</div>
             <div className="underline"></div>
         </div>
         <div className="inputs">
-            <div className="input">
+            {action==="Login"?<div></div>:<div className="input">
                 <img src={user_icon} alt="" />
-                <input type="text" />
+                <input type="text" placeholder="Name" />
+                </div>}
+            
+                <div className="input">
+                <img src={email_icon} alt="" />
+                <input type="email" placeholder="Email Address" />
                 </div>
-                <div className={email_icon}>
-                <img src="" alt="" />
-                <input type="email" />
-                </div>
-                <div className={password_icon}>
-                <img src="" alt="" />
-                <input type="password" />
+                <div className="input">
+                <img src={password_icon} alt="" />
+                <input type="password" placeholder="Password"/>
             </div>
         </div>
-        <div className="forgot-password">Lost Password? <span>Click Here</span></div>
+        {action==="Sign Up"?<div></div>: <div className="forgot-password">Lost Password? <span>Click Here</span></div>}
+       
         <div className="submit-container">
-            <div className="submit">Sign Up</div>
-            <div className="submit">Login</div>
+            <div className={action==="Login"?"submit gray":"submit"} onClick={() => {setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit gray":"submit"} onClick={() => {setAction("Login")}}>Login</div>
         </div>
      </div>
   )
