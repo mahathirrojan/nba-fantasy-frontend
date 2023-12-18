bd264882a031b620d8854ceacd96608ff5f9105e
Abir12918 <banikabir1234@gmail.com>
2023-12-18 13:58:35 -0500

    Player Details Working

--- nba-fantasy/src/Components/Sports/PlayerDetails.js
+++ nba-fantasy/src/Components/Sports/PlayerDetails.js
@@ -1,46 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setPlayerDetails } from '../../actions'; 
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { setPlayerDetails } from '../../actions'; 
 
 
-const PlayerDetails = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const playerDetails = useSelector(state => state.playerDetails); 
+// const PlayerDetails = () => {
+//   const { id } = useParams();
+//   const dispatch = useDispatch();
+//   const playerDetails = useSelector(state => state.playerDetails); 
 
 
   
 
 
-  useEffect(() => {
-    const fetchPlayerDetails = async () => {
-      try {
-        const response = await fetch(`https://www.balldontlie.io/api/v1/players/${id}`);
-        const data = await response.json();
+//   useEffect(() => {
+//     const fetchPlayerDetails = async () => {
+//       try {
+//         const response = await fetch(`https://www.balldontlie.io/api/v1/players/${id}`);
+//         const data = await response.json();
 
-        dispatch(setPlayerDetails(data));
-      } catch (error) {
-        console.error('Error fetching player details:', error);
-      }
-    };
+//         dispatch(setPlayerDetails(data));
+//       } catch (error) {
+//         console.error('Error fetching player details:', error);
+//       }
+//     };
 
-    fetchPlayerDetails();
-  }, [id, dispatch]);
+//     fetchPlayerDetails();
+//   }, [id, dispatch]);
 
-  if (!playerDetails) {
-    return <div>Loading...</div>;
-  }
+//   if (!playerDetails) {
+//     return <div>Loading...</div>;
+//   }
 
-  return (
-    <div>
-      <h2>{playerDetails.first_name} {playerDetails.last_name}</h2>
-      <p>Height: {playerDetails.height_feet}' {playerDetails.height_inches}"</p>
-      <p>Weight: {playerDetails.weight_pounds} lbs</p>
-      <p>Position: {playerDetails.position}</p>
-      <p>Team: {playerDetails.team.full_name}</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>{playerDetails.first_name} {playerDetails.last_name}</h2>
+//       <p>Height: {playerDetails.height_feet}' {playerDetails.height_inches}"</p>
+//       <p>Weight: {playerDetails.weight_pounds} lbs</p>
+//       <p>Position: {playerDetails.position}</p>
+//       <p>Team: {playerDetails.team.full_name}</p>
+//     </div>
+//   );
+// };
 
-export default PlayerDetails;
+// export default PlayerDetails;
